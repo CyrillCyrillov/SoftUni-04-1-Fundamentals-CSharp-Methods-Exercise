1999e3160bdf07e8cc67d89d345e6f842ba5ac20
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:30:17 +0200

    Add files via upload

---
+++ Task02_Vowels_Count/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task02_Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            int vowelsNumbers = GetVowelsNumbers(inputText);
+
+            Console.WriteLine(vowelsNumbers);
+        }
+
+        static int GetVowelsNumbers(string inputText)
+        {
+            string vowelLetters = "AaEeOoIiUu";
+            int vowelsNumbers = 0;
+            for (int j = 0; j < inputText.Length; j++)
+            {
+                char nextLetter = inputText[j];
+                if (vowelLetters.Contains(nextLetter))
+                {
+                    vowelsNumbers += 1;
+                }
+            }
+
+            return vowelsNumbers;
+        }
+    }
+}
