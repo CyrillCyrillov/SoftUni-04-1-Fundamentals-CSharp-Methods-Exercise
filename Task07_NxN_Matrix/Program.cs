87a3f884d74ba09a5e3a6703b630ed38da6fd789
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:35:27 +0200

    Add files via upload

---
+++ Task07_NxN_Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task07_NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintMatrix(n);
+            
+            //Console.WriteLine("Hello World!");
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write($"{n} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
