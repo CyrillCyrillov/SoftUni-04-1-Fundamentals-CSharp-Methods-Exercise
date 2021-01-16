7160cc5658b94eeab1bb374c63904b41f0abfd38
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:31:49 +0200

    Add files via upload

---
+++ Task03_Characters_in_Range/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task03_Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char begin = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            
+            PrintCharacters(begin, end);
+        }
+        
+        static void PrintCharacters(char begin, char end)
+        {
+            if(begin > end)
+            {
+                char help = end;
+                end = begin;
+                begin = help;
+            }
+            for (int i = begin + 1; i < end; i++)
+            {            
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
