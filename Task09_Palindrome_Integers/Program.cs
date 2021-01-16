a617d9c56ffcd9372107706db664dadf72122898
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:37:18 +0200

    Add files via upload

---
+++ Task09_Palindrome_Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task09_Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber = Console.ReadLine();
+            //bool isPalindrom = true;
+
+            while (inputNumber != "END")
+            {
+                Console.WriteLine(IsPalindrom(inputNumber).ToString().ToLower());
+
+                inputNumber = Console.ReadLine();
+            }
+        }
+
+        private static bool IsPalindrom(string inputNumber)
+        {
+            string inversNumber = string.Empty;
+            for (int i = inputNumber.Length - 1; i >= 0; i--)
+            {
+                inversNumber += inputNumber[i];
+            }
+
+            if(inputNumber == inversNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
