d0175d3c7fb8c89b7d16650c6be41b5fcc35dfc2
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:34:32 +0200

    Add files via upload

---
+++ Task06_Middle_Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task06_Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(MidelOfText(text));
+            
+            //Console.WriteLine("Hello World!");
+        }
+
+        private static string MidelOfText(string text)
+        {
+            if(text.Length % 2 != 0)
+            {
+                return text[text.Length / 2].ToString();
+            }
+            else
+            {
+                return text[(text.Length / 2) - 1].ToString() + text[(text.Length / 2)].ToString();
+            }
+        }
+    }
+}
