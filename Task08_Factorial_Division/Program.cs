a4385608272db3a1ead61ae9c03137685584d0d6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:36:29 +0200

    Add files via upload

---
+++ Task08_Factorial_Division/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task08_Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"{Faktorial(number1) / Faktorial(number2):f2}");
+        }
+
+        private static double Faktorial(int number)
+        {
+            double result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
