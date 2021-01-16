04a7c6748c30266f31d038b6a7f9419206d3d7ff
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:33:44 +0200

    Add files via upload

---
+++ Task05_Add_and_Subtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task05_Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            int sum = AddNumbers(number1, number2);
+            int substract = Substract(sum, number3);
+
+            Console.WriteLine(substract);
+        }
+
+        private static int Substract(int sum, int number3)
+        {
+            return sum - number3;
+        }
+
+        private static int AddNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+
+    }
+}
